0f7155d788d629c7d2437955d70a78bfe2fe1d75
Kirke Ruusalu <kirke.ruusalu@gmail.com>
2023-04-04 22:38:42 +0300

    main.py modified

--- src/main.py
+++ src/main.py
@@ -3,7 +3,19 @@ from repositories.user_repository import UserRepository
 from initialize_database import initialize_database
 from services.calculator_service import CalculatorService
 
-repository = UserRepository
-calculator = CalculatorService
+repository = UserRepository()
+calculator = CalculatorService()
 
 username = str(input("username:"))
+
+calculator.create_user(username)
+
+print("new user created")
+
+print("here are all the current users: ", repository.find_all_users())
+
+if str(input("do you want to find by username? (y/n)")) == "y":
+    name = str(input("enter username to look for: "))
+    print(repository.find_by_username(name))
+ 
+    
